3926d253e507541587e892b93e483da538ae6f10
Alvin Pradana Antony <74761484+AlvinPradanaAntony@users.noreply.github.com>
2021-03-16 10:46:06 +0700

    Add files via upload

---
+++ Minggu 2/Latihan/Latihan_A.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_a;
+
+/**
+ *
+ * @author Tony's PC
+ */
+public class Latihan_A {
+
+    /**
+     Belajar menampilkan hasil output dengan code berikut :
+     * System.out.println(""); 
+        Code ini membuat sebuah outputan dengan
+        menerapkan enter atau penambahan baris pada kalimat berikutnya.
+     *
+     * System.out.print(""); 
+        Code ini membuat sebuah outputan tanpa menerapkan
+        enter atau penambahan baris pada kalimat berikutnya.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+        System.out.println("World !");
+
+        System.out.println("");
+
+        System.out.print("Hello ");
+        System.out.print("World !");
+
+    }
+
+}
