6fc690d7e739fedecb5ac8c3a05d0ebf55b43c76
Alvin Pradana Antony <74761484+AlvinPradanaAntony@users.noreply.github.com>
2021-03-16 10:48:01 +0700

    Latihan Minggu 2

---
+++ Minggu 2/Latihan/Latihan_B.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_b;
+
+/**
+ *
+ * @author Tony's PC
+ */
+public class Latihan_B {
+
+    /**
+      Belajar menampilkan hasil output dari sebuah variable dengan
+      deklarasi dan inisialisasi.
+    
+     */
+    public static void main(String[] args) {
+       String nama = "Indonesia";
+       char abjad = 'I';
+       int a = 9;
+       double b = 8.9;
+       
+        System.out.println("Nama Negara = "+ nama);
+        System.out.println("Karakter Awal = " + abjad);
+        System.out.println("Jumlah karakter = " + a);
+        System.out.println("Bilangan desimal = " + b);
+    }
+    
+}
---
+++ Minggu 2/Latihan/Latihan_C.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_c;
+/**
+ *
+ * @author Tony's PC
+ * Belajar mengenai class, object, dan method
+ */
+class buku {
+    String judul;
+    int harga;
+    int halaman;   
+    public void InfoBuku(String judul, int harga, int halaman) {  
+        this.judul = judul;
+        this.harga = harga;
+        this.halaman = halaman;       
+    }
+    public void outputInfoBuku() {
+        
+        System.out.println("Informasi Buku");
+        System.out.println("Judul Buku = " + judul);
+        System.out.println("Harga buku : " + harga);
+        System.out.println("Jumlah halaman : " + halaman);     
+    }   
+}
+public class Latihan_C {
+    public static void main(String[] args) {
+        
+        buku buku_java = new buku();
+        buku_java.InfoBuku("buku java", 60000, 120);
+        buku_java.outputInfoBuku();        
+    }
+}
