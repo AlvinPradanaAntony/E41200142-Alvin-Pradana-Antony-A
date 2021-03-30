2e757ce67828a8728396e08a18150b95f8e47af4
Alvin Pradana Antony <74761484+AlvinPradanaAntony@users.noreply.github.com>
2021-03-30 18:06:40 +0700

    Latihan Minggu 4

---
+++ Minggu 4/Latihan/Latihan_B_StatementDoWhile.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_minggu4;
+
+/**
+ *
+ * @author Tony's PC
+ */
+public class Latihan_B_StatementDoWhile {
+
+    // Statement Do - While
+    public static void main(String[] args) {
+        // membuat inisialisasi variabel
+        int i = 0;
+
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while ( i <= 10);
+        
+    }
+
+}
