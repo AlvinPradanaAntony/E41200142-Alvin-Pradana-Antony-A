3e9eb5861d5af892c24f38a1166609270da7720c
Alvin Pradana Antony <74761484+AlvinPradanaAntony@users.noreply.github.com>
2021-03-30 19:24:37 +0700

    Latihan Minggu ke 4

---
+++ Minggu 4/Latihan/Latihan_D_StatementForEach.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_minggu4;
+
+/**
+ *
+ * @author Tony's PC
+ */
+public class Latihan_D_StatementForEach {
+
+    // Statement - ForEach
+    public static void main(String[] args) {
+        // Membuat array
+        int angka[] = {3, 1, 4, 24, 12};
+
+        // Menggunakan perulangan For each untuk menampilkan angka
+        for (int x : angka) {
+            System.out.print(x + " ");
+        }
+    }
+
+}
